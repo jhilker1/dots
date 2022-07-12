77059a2af548052e209a307d9f60415c524b87a8
Jacob Hilker <jacob.hilker2@gmail.com>
2022-07-12 12:50:52 -0400

    added emoji and biblio to doom

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,1 +1,6 @@
 (setq doom-font (font-spec :name "Josevka Code" :size 16))
+
+(after! org
+  (add-to-list 'org-babel-default-header-args
+               '((:noweb . "yes")
+                 (:mkdirp . "yes"))))

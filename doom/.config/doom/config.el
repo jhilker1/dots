cf618eac870eb58f3e4309c640e32d797d16c5b9
Jacob Hilker <jacob.hilker2@gmail.com>
2022-07-12 17:15:14 -0400

    added Iosevka build script

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,6 +1,1 @@
 (setq doom-font (font-spec :name "Josevka Code" :size 16))
-
-(after! org
-  (add-to-list 'org-babel-default-header-args
-               '((:noweb . "yes")
-                 (:mkdirp . "yes"))))

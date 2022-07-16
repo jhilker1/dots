4a34f17e1b7901562d44698c9a32285fb2cffd55
Jacob Hilker <jacob.hilker2@gmail.com>
2022-07-15 21:19:30 -0400

    added org-hugo and roam

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,1 +1,5 @@
 ;(setq doom-font (font-spec :name "Josevka Code" :size 16))
+
+(after! org-roam
+  (setq org-roam-directory "/mnt/c/Users/camoh/Dropbox/roam"
+        org-roam-db-location "~/.org-roam.db"))

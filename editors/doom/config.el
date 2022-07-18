53bf75891857b5c2ea62852569d626774d98bcb2
Jacob Hilker <jacob.hilker2@gmail.com>
2022-07-18 11:02:20 -0400

    added doom font

--- editors/doom/config.el
+++ editors/doom/config.el
@@ -1,4 +1,4 @@
-(setq doom-font (font-spec :name "Josevka Code" :size 16))
+;(setq doom-font (font-spec :name "Josevka Code" :size 16))
 
 (after! org-roam
   (setq org-roam-directory "/mnt/c/Users/camoh/Dropbox/roam"

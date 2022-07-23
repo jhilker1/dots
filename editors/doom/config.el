748b32000ac9b39b52f00b759a476bacb6fa7f79
Jacob Hilker <jacob.hilker2@gmail.com>
2022-07-22 22:28:31 -0400

    setup org-roam namespace face

--- editors/doom/config.el
+++ editors/doom/config.el
@@ -74,15 +74,16 @@
 (after! org-roam
   (setq org-roam-directory "~/roam/"
         org-roam-db-location "~/.org-roam.db"
+        org-roam-db-autosync-mode t
         org-roam-completion-everywhere t)
+
   (cl-defmethod org-roam-node-namespace ((node org-roam-node))
     "Return the currently set namespace for the NODE."
     (let ((namespace (cdr (assoc-string "NAMESPACE" (org-roam-node-properties node)))))
       (if (string= namespace (file-name-base (org-roam-node-file node)))
           "" ; or return the current title, e.g. (org-roam-node-title node)
         (format "%s" namespace))))
-
-  (setq org-roam-node-display-template (concat "${namespace:15} ${title:*}" (propertize "${doom-tags:50}" 'face 'org-tag))))
+  (setq org-roam-node-display-template (concat (propertize " ${namespace:15} " 'face '(:foreground "#d3869b" :weight bold)) "${title:*}" (propertize "${doom-tags:50}" 'face 'org-tag))))
 
 (after! vulpea
   (add-hook! 'org-roam-db-autosync-mode #'vulpea-db-autosync-enable))
